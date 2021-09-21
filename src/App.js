c5e69e8fe76dd4cf09f9213bb4371a732ff84903
Tejas Ladhani <67834407+Tejas-Ladhani@users.noreply.github.com>
2021-09-21 19:19:07 +0530

    antd layout basic

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import {
   DesktopOutlined,
   PieChartOutlined,
   FileOutlined,
-  TeamOutlined,
   UserOutlined,
 } from '@ant-design/icons';
 import {useState} from 'react';
 
 const { Header, Content, Footer, Sider } = Layout;
-const { SubMenu } = Menu;
 
 function App() {
   const [collapsed, setcollapsed] = useState(false)
@@ -23,25 +21,19 @@ function App() {
   return (
     <Layout style={{ minHeight: '100vh' }}>
     <Sider collapsible collapsed={collapsed} onCollapse={()=> onCollapse(!collapsed)}>
-      <div className="logo" />
+      <div className="logo"/>
       <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
         <Menu.Item key="1" icon={<PieChartOutlined />}>
-          Option 1
+          Dashboard
         </Menu.Item>
         <Menu.Item key="2" icon={<DesktopOutlined />}>
-          Option 2
+          Monitor applications
         </Menu.Item>
-        <SubMenu key="sub1" icon={<UserOutlined />} title="User">
-          <Menu.Item key="3">Tom</Menu.Item>
-          <Menu.Item key="4">Bill</Menu.Item>
-          <Menu.Item key="5">Alex</Menu.Item>
-        </SubMenu>
-        <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
-          <Menu.Item key="6">Team 1</Menu.Item>
-          <Menu.Item key="8">Team 2</Menu.Item>
-        </SubMenu>
-        <Menu.Item key="9" icon={<FileOutlined />}>
-          Files
+        <Menu.Item key="3" icon={<FileOutlined />}>
+          Documents
+        </Menu.Item>
+        <Menu.Item key="4" icon={<UserOutlined />} title="User">
+         Profile  
         </Menu.Item>
       </Menu>
     </Sider>
