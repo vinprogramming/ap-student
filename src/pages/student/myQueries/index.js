790a4fda38dc27e8753caf93cdd2360308ebb958
Mr-Developer2204 <59203865+Mr-Developer2204@users.noreply.github.com>
2021-10-06 10:04:46 +0530

    Nonfunctioning Queries Page with dummy data

--- src/pages/student/myQueries/index.js
+++ src/pages/student/myQueries/index.js
@@ -1,76 +1,121 @@
 import React from "react";
 import "./style.css";
-import { Layout, Row, Tabs, Col, Card, Typography,Button } from "antd";
-import {PlusOutlined} from '@ant-design/icons';
+import { Layout, Row, Tabs, Col, Card, Typography, Button } from "antd";
+import { PlusCircleOutlined } from "@ant-design/icons";
 // import QueryTabs from '../../../components/QueryTabs';
 var tabkey = 0;
 const { TabPane } = Tabs;
+const createnewQuery = (
+  <Button
+    id="createquery"
+    type="primary"
+    shape="round"
+    icon={<PlusCircleOutlined />}
+    size="medium"
+  >
+    Create a Query
+  </Button>
+);
 export default function MyQueries() {
   return (
     <div className="myquery">
       <Layout>
         <Row>
-          <Col span={24}>
-            <div className="">
-              <Typography.Title level={1}>My Queries</Typography.Title>
+          <Col span={20}>
+            <div className="myquery_TopTitle">
+              <Typography.Title level={2}>My Queries</Typography.Title>
             </div>
           </Col>
+          {/* <Col span={4} offset={18}>
+            <Button
+              id="createquery"
+              type="primary"
+              shape="round"
+              icon={<PlusCircleOutlined />}
+              size="large"
+            >
+              Create a Query
+            </Button>
+          </Col> */}
+          <Col span={23}>
+            <Tabs tabBarExtraContent={createnewQuery}>
+              <TabPane tab="All Queries" key={tabkey++}>
+                <Row>
+                  <Col span={24}>
+                    <div className="querycard">
+                      <Card>
+                        <Row>
+                          <Col span={24}>
+                            <div className="">
+                              <Typography.Title level={4}>
+                                Query 2
+                              </Typography.Title>
+                            </div>
+                          </Col>
+                        </Row>
+                        <Row>
+                          <Col span={24}>
+                            <div className="">
+                              <Typography.Title level={5}>
+                                Query 2 Description
+                              </Typography.Title>
+                            </div>
+                          </Col>
+                        </Row>
+                      </Card>
+                    </div>
+                  </Col>
+                </Row>
+              </TabPane>
+              <TabPane tab="Solved" key={tabkey++}>
+                <Row>
+                  <Col span={24}>
+                    <div className="querycard">
+                      <Card>
+                        <Row>
+                          <Col span={24}>
+                            <div className="">
+                              <Typography.Title level={4}>
+                                Solved
+                              </Typography.Title>
+                            </div>
+                          </Col>
+                        </Row>
+                      </Card>
+                    </div>
+                  </Col>
+                </Row>
+              </TabPane>
+              <TabPane tab="Pending" key={tabkey++}>
+                <Row>
+                  <Col span={24}>
+                    <div className="querycard">
+                      <Card title='Subject' size='small'>
+                        <Row>
+                          <Col span={12}>
+                            <div className="cardcontent">
+                              <Typography.Text strong>this is the description of the query that you have submitted</Typography.Text>
+                            </div>
+                          </Col>
+                          <Col span={4} offset={2}>
+                            <div className="cardcontent">
+                              <Typography.Text keyboard type='danger' >Status</Typography.Text>
+                            </div>
+                          </Col>
+                          <Col span={4} offset={2}>
+                            <div className="cardcontent">
+                              <Typography.Text type='secondary'>Assigned TO</Typography.Text>
+                            </div>
+                          </Col>
+                        </Row>
+                      </Card>
+                    </div>
+                  </Col>
+                </Row>
+              </TabPane>
+            </Tabs>
+          </Col>
         </Row>
-        
-        <Tabs>
-          <TabPane tab="All Queries" key={tabkey++}>
-            <Row>
-              <Col span={24}>
-                <div className="">
-                  <Card>
-                    <Row>
-                      <Col span={24}>
-                        <div className="">
-                          <Typography.Title level={4}>Query 2</Typography.Title>
-                        </div>
-                      </Col>
-                    </Row>
-                  </Card>
-                </div>
-              </Col>
-            </Row>
-          </TabPane>
-          <TabPane tab="Solved" key={tabkey++}>
-            <Row>
-              <Col span={24}>
-                <div className="">
-                  <Card>
-                    <Row>
-                      <Col span={24}>
-                        <div className="">
-                          <Typography.Title level={4}>Solved</Typography.Title>
-                        </div>
-                      </Col>
-                    </Row>
-                  </Card>
-                </div>
-              </Col>
-            </Row>
-          </TabPane>
-          <TabPane tab="Pending" key={tabkey++}>
-            <Row>
-              <Col span={24}>
-                <div className="">
-                  <Card>
-                    <Row>
-                      <Col span={24}>
-                        <div className="">
-                          <Typography.Title level={4}>Query 2</Typography.Title>
-                        </div>
-                      </Col>
-                    </Row>
-                  </Card>
-                </div>
-              </Col>
-            </Row>
-          </TabPane>
-        </Tabs>
-        
       </Layout>
     </div>
   );
