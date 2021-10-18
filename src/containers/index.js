a57c24107758310895659ca6ae7665ee8323ec7b
Tejas Ladhani <67834407+Tejas-Ladhani@users.noreply.github.com>
2021-10-18 17:24:08 +0530

    Form with steps (#21)
    * basic form with steps
    
    * basic MutliStepForm
    
    * FormWithStep v0

--- src/containers/index.js
+++ src/containers/index.js
@@ -4,3 +4,4 @@ export { default as ProfileEdit } from './ProfileEdit';
 export { default as ProfileOther } from './ProfileOther';
 export { default as ProfilePassAndSec } from './ProfilePassAndSec';
 export {default as ApplicationCard} from './ApplicationCard';
+export { default as FormWithStep } from '../containers/FormWithStep';
