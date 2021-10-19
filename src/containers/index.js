fe5a79009e386976cc33a987db6a9cb46bddec17
Tejas Ladhani <67834407+Tejas-Ladhani@users.noreply.github.com>
2021-10-19 23:42:03 +0530

    Profile & Related changed

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,5 +1,6 @@
 export { default as UpdateContainer } from './UpdateContainer';
 export { default as ProfilePicCard } from './ProfilePicCard';
+export { default as ProfileDisplay } from './ProfileDisplay';
 export { default as ProfileEdit } from './ProfileEdit';
 export { default as ProfileOther } from './ProfileOther';
 export { default as ProfilePassAndSec } from './ProfilePassAndSec';
